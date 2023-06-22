097da27816fab6d57efce339ed9bb6731da3e2f0
ronangr1 <guerin.ronan@aol.com>
2023-06-22 13:41:11 +0200

    File cleanup

--- view/frontend/web/js/widget/halt.js
+++ view/frontend/web/js/widget/halt.js
@@ -17,15 +17,8 @@ define(
                 },
 
                 _create: function () {
-                    const self = this
                     if (this.options.isEnabled !== "0") {
                         this._init()
-                        this.default.tries.subscribe(function (t) {
-                            if (t >= 20) {
-                                $(self.options.element).removeAttr("style")
-                                self.default.tries(0)
-                            }
-                        })
                     }
                 },
 
@@ -37,7 +30,7 @@ define(
                             self._randomPosition($(this))
                         })
                         // Prevent click events from any source
-                        el.click(function(e) {
+                        el.click(function (e) {
                             e.preventDefault()
                         })
                         // Prevent tab navigation or smart customer keyboard navigation
@@ -47,16 +40,29 @@ define(
                     }
                 },
 
+                /**
+                 * Add css properties to replace the current location of an element
+                 * After ~20 tries, the element is going back to its first position
+                 * @param element
+                 * @private
+                 */
                 _randomPosition: function (element) {
-                    const properties = {
-                        'position': 'absolute',
-                        'top': Math.floor((Math.random() * this.default.bodyPosY)),
-                        'left': Math.floor((Math.random() * this.default.bodyPosX)),
-                        'transition': '.15s ease-out',
-                        'z-index': '1000'
-                    }
+                    const self = this,
+                        properties = {
+                            'position': 'absolute',
+                            'top': Math.floor((Math.random() * this.default.bodyPosY)),
+                            'left': Math.floor((Math.random() * this.default.bodyPosX)),
+                            'transition': '.15s ease-out',
+                            'z-index': '1000'
+                        }
                     element.css(properties)
                     this.default.tries(this.default.tries() + 1)
+                    this.default.tries.subscribe(function (t) {
+                        if (t >= 20) {
+                            $(self.options.element).removeAttr("style")
+                            self.default.tries(0)
+                        }
+                    })
                 }
             }
         );
